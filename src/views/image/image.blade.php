526cd63c48bf571fd9ce4caefad17926d0199c08
Ezra Pool <ezra@digitalnatives.nl>
2024-04-24 11:28:40 +0200

    feat: add lazy loading and other performance improvements to st images

--- src/views/image/image.blade.php
+++ src/views/image/image.blade.php
@@ -1,5 +1,5 @@
 <figure class="st-image">
-  <img src="{{ $url }}" alt="" />
+  <img src="{{ $url }}" alt="" loading="lazy" decoding="async" fetchPriority="low" />
 
 @if (!empty($text))
   <figcaption>{{ $text }}</figcaption>
